605fe4cbefa741e4066817e7c45bc7899d2790a9
Nathnael Hailu Tsegaye <nht249@nyu.edu>
2023-03-19 09:31:45 +0400

    change initial guess for gainDBSPL

--- volume.py
+++ volume.py
@@ -158,7 +158,7 @@ def get_model_parameters(inDB,outDBSPL,lCalibFromPeer):
     global lCalib
     lCalib = lCalibFromPeer
     maxMeasuredDBSPL = np.max(outDBSPL)
-    guesses=[70,130,maxMeasuredDBSPL,100,20]
+    guesses=[70,100,maxMeasuredDBSPL,100,20]
     guesses=scipy.optimize.fmin(SoundLevelCost,guesses,args=(inDB,outDBSPL))
     rmsError = CalculateRMSError(inDB,outDBSPL,guesses[0],guesses[1],guesses[2],guesses[3],guesses[4])
     return guesses[0], guesses[1], guesses[2], guesses[3], guesses[4], rmsError #backgroundDBSPL,gainDBSPL,T,R,W,rmsError
